478ce8c71934663e5267e8192e8a5f45e83465ae
Filippenko Pavel <filippenko.ps@phystech.edu>
2024-10-29 17:15:18 +0300

    only ansible implementation

--- Lab02/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker rm $(docker ps -a -q)
-docker build -t python_udp_server_image -f Dockerfile_server .
-docker build -t python_udp_client_image -f Dockerfile_client .
-docker build -t ansible_image -f Dockerfile_ansible .
